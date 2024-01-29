e87834f05b479bea9657c1e975d5e35971d7c179
jamesiwat-earnin <siwat.kaolueng@earnin.com>
2024-01-29 14:04:47 +0700

    type data

--- src/services/catService.ts
+++ src/services/catService.ts
@@ -4,6 +4,8 @@ import type { paths } from "../types/cat-fact.ts";
 
 const { GET } = createClient<paths>({ baseUrl: "https://catfact.ninja" });
 
+export type Data<T extends keyof paths> = paths[T]["get"]["responses"][200]["content"]["application/json"];
+export type CatFact = Data<"/fact">
 
 export async function getCatFact() {
     const { data, error } = await GET("/fact", {
@@ -18,3 +20,6 @@ export async function getCatFact() {
     }
     return data
 }
+
+
+export type Breed = Data<"/breeds">
