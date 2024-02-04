a215f1d3275ad3938beaca5cac12f83b4a4bb326
jamesiwat-earnin <siwat.kaolueng@earnin.com>
2024-02-04 16:34:11 +0700

    fix fix tree

--- src/components/tree/tree.tsx
+++ src/components/tree/tree.tsx
@@ -5,39 +5,170 @@ interface Node {
     children?: Node[]
 
 }
+
+export function generateNxNTree(/* TODO */...args: any[]): Node[] {
+    // TODO: Implement this function
+    const result: Node[] = [{
+
+        label: "Node 1",
+        children: [
+            {
+                label: "Node 1.1",
+                children: [
+                    {
+                        label: "Node 1.1.1",
+                    },
+                    {
+                        label: "Node 1.1.2",
+                    },
+                    {
+                        label: "Node 1.1.3",
+                    },
+                ],
+            },
+            {
+                label: "Node 1.2",
+                children: [
+                    {
+                        label: "Node 1.2.1",
+                    },
+                    {
+                        label: "Node 1.2.2",
+                    },
+                    {
+                        label: "Node 1.2.3",
+                    },
+                ],
+            },
+            {
+                label: "Node 1.3",
+                children: [
+                    {
+                        label: "Node 1.3.1",
+                    },
+                    {
+                        label: "Node 1.3.2",
+                    },
+                    {
+                        label: "Node 1.3.3",
+                    },
+                ],
+            },
+        ],
+    }, {
+        label: "Node 2",
+        children: [
+            {
+                label: "Node 2.1",
+                children: [
+                    {
+                        label: "Node 2.1.1",
+                    },
+                    {
+                        label: "Node 2.1.2",
+                    },
+                    {
+                        label: "Node 2.1.3",
+                    },
+                ],
+            },
+            {
+                label: "Node 2.2",
+                children: [
+                    {
+                        label: "Node 2.2.1",
+                    },
+                    {
+                        label: "Node 2.2.2",
+                    },
+                    {
+                        label: "Node 2.2.3",
+                    },
+                ],
+            },
+            {
+                label: "Node 2.3",
+                children: [
+                    {
+                        label: "Node 2.3.1",
+                    },
+                    {
+                        label: "Node 2.3.2",
+                    },
+                    {
+                        label: "Node 2.3.3",
+                    },
+                ],
+            },
+        ],
+    },
+        {
+            label: "Node 3",
+            children: [
+                {
+                    label: "Node 3.1",
+                    children: [
+                        {
+                            label: "Node 3.1.1",
+                        },
+                        {
+                            label: "Node 3.1.2",
+                        },
+                        {
+                            label: "Node 3.1.3",
+                        },
+                    ],
+                },
+                {
+                    label: "Node 3.2",
+                    children: [
+                        {
+                            label: "Node 3.2.1",
+                        },
+                        {
+                            label: "Node 3.2.2",
+                        },
+                        {
+                            label: "Node 3.2.3",
+                        },
+                    ],
+                },
+                {
+                    label: "Node 3.3",
+                    children: [
+                        {
+                            label: "Node 3.3.1",
+                        },
+                        {
+                            label: "Node 3.3.2",
+                        },
+                        {
+                            label: "Node 3.3.3",
+                        },
+                    ],
+                },
+            ]
+        }
+    ];
+    return result;
+}
+
 interface Props {
     node: Node,
 }
 
-// generate mega tree with n levels of depth and 10 children per node with label "Node 1.2.3"
-export function generateMegaTree(maxDepth: number, depth = 0, label = '1'): Node {
-    if (depth === maxDepth) {
-        return { label: `Node ${label} (Leaf)` };
-    }
-
-    const children = [];
-    for (let i = 1; i < 3; i++) {
-        children.push(generateMegaTree(maxDepth, depth + 1, `${label}.${i}`));
-    }
-    return {
-        label: `Node ${label}`,
-        children,
-    };
-
-}
-export const bigData = generateMegaTree(10);
-
-const TreeNode = ({ node }: Props) => {
+const TreeNode = ({node}: Props) => {
     const [expanded, setExpanded] = useState(true); //
-    const { label, children } = node;
+    const {label, children} = node;
 
     return (
         <ul className="pl-2 m-[unset] list-disc">
-            <li className="pl-2 m-[unset] list-disc cursor-pointer" key={label} onClick={() => setExpanded(prev => !prev)}>{label}</li>
+            <li className="pl-2 m-[unset] list-disc cursor-pointer" key={label}
+                onClick={() => setExpanded(prev => !prev)}>{label}</li>
             {children && children.length > 0 && expanded && (
                 <ul>
                     {children.map((child) => (
-                        <TreeNode key={child.label} node={child} />
+                        <TreeNode key={child.label} node={child}/>
                     ))}
                 </ul>
             )}
@@ -46,8 +177,15 @@ const TreeNode = ({ node }: Props) => {
 };
 
 interface TreeProps {
-    data: Node
+    data: Node[]
 }
-export const Tree = ({ data }: TreeProps) => {
-    return <TreeNode node={data} />;
+
+export const Tree = ({data}: TreeProps) => {
+    return (
+        <div>
+            {data.map((node) => (
+                <TreeNode key={node.label} node={node}/>
+            ))}
+        </div>
+    );
 };
